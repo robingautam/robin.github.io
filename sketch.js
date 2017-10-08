aa2e2d36cbe7dd31e9d559a026f709bc74ab46fa
lokesh <lokeshmangal70@gmail.com>
2017-10-08 22:40:59 +0530

    2nd
    2nd commit

---
+++ sketch.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $('#rob').slideUp(function() {
+        $(this).show('5000');
+    });
+ $('#menu1').ready(function() {
+    $('#menu1').slideUp(function() {
+        $(this).show(5000);
+    });
+});
+$('#menu2').ready(function() {
+    $('#menu2').slideUp(function() {
+        $(this).show(5000);
+    });
+});
+    $('#m1').ready(function() {
+    $('#m1').slideUp(function() {
+        $(this).show(15000);
+    });
+});
+$('#m2').ready(function() {
+    $('#m2').slideUp(function() {
+        $(this).show(15000);
+    });
+});
+    $('#m3').ready(function() {
+    $('#m3').slideUp(function() {
+        $(this).show(15000);
+    });
+});
+    $('#m4').ready(function() {
+    $('#m4').slideUp(function() {
+        $(this).show(15000);
+    });
+});
+    $('#table').ready(function() {
+        $('#table').slideUp(function() {
+            $(this).show(30000);
+        });
+    });
+    $('#rob1').ready(function() {
+        $('#rob1').slideUp(function() {
+            $(this).show(25000);
+        });
+    });
+    $('#rob2').ready(function() {
+        $('#rob2').slideUp(function() {
+            $(this).show(25000);
+        });
+    });
+    
+    });
